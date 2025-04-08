0f41f98d4a62ba4c28bba9eb0e13d30108bd3ae8
Rodrigo Ramirez <rodrigopinheiroramirez@gmail.com>
2025-04-08 11:03:15 -0300

    Adicionando arquivos de configuração

---
+++ src/test/java/com/rramirez/animals_service/AnimalsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rramirez.animals_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

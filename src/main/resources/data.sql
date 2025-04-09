a31e0d65771f85a435976c295ce8d81825003dd3
Rodrigo Ramirez <rodrigopinheiroramirez@gmail.com>
2025-04-09 10:28:19 -0300

    inserindo dados na tabela.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES ('Bob', 1, 'Pinscher', NOW(), 'Com pulgas', 'Rub', 'Pequeno');

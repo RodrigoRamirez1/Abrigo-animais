4e99c275acc54ad194b7d929a9008403bc9c5085
Rodrigo Ramirez <rodrigopinheiroramirez@gmail.com>
2025-04-10 10:50:10 -0300

    Add no repositorio os metodos de Listar os adotados e os não adotados.

--- src/main/java/com/rramirez/animals_service/repositorios/AnimalReposiory.java
+++ src/main/java/com/rramirez/animals_service/repositorios/AnimalReposiory.java
@@ -2,6 +2,14 @@ package com.rramirez.animals_service.repositorios;
 
 import com.rramirez.animals_service.entidades.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AnimalReposiory extends JpaRepository<Animal, Long> {
+    @Query("SELECT a FROM Animal a WHERE a.dataAdocao IS NULL ORDER BY a.dataEntrada")
+    List<Animal> findNotAdoped();
+
+    @Query("SELECT a FROM Animal a WHERE a.dataAdocao IS NOT NULL ORDER ")
+    List<Animal> findAdoped();
 }

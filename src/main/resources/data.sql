d564986aa9db77940ac2e37035bb19d90f686020
Rodrigo Ramirez <rodrigopinheiroramirez@gmail.com>
2025-04-10 16:46:55 -0300

    Inserindo valores a tabela animal.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,14 @@
-INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
-VALUES ('Bob', 1, 'Pinscher', NOW(), 'Com pulgas', 'Rub', 'Pequeno');
+INSERT INTO animal (tipo_animal, nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES ('CACHORRO','Bob', 1, 'Pinscher', NOW(), 'Com pulgas', 'Rub', 'Pequeno');
+
+INSERT INTO animal (tipo_animal, nome_provisorio, idade_estimada, raca, data_entrada, data_adocao, condicoes_chegada, nome_recebedor, porte)
+VALUES ('CACHORRO', 'Lola', 8, 'Pinscher', NOW() - 10, NOW(), 'Com pulgas', 'Pity', 'Pequeno');
+
+INSERT INTO animal (tipo_animal, nome_provisorio, idade_estimada, raca, data_entrada, data_adocao, condicoes_chegada, nome_recebedor, porte)
+VALUES ('CACHORRO', 'Leo', 2, 'Pinscher', NOW() - 30, NOW() - 10, 'Com pulgas', 'Rub', 'Pequeno');
+
+INSERT INTO animal (tipo_animal, nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES ('GATO', 'Pandora', 2, 'Siames', NOW(), 'Com pulgas e vermes', 'Pity', 'Pequeno');
+
+INSERT INTO animal (tipo_animal, nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES ('GATO', 'Kaki', 1, 'Siames', NOW(), 'Com pulgas e vermes', 'Rub', 'Pequeno');

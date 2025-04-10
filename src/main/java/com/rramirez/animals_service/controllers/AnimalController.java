f03558ead9044c98f654214db0ef8edd1d3778e6
Rodrigo Ramirez <rodrigopinheiroramirez@gmail.com>
2025-04-10 16:52:11 -0300

    Adicionando gets por cachorro, gatos e resgates .

--- src/main/java/com/rramirez/animals_service/controllers/AnimalController.java
+++ src/main/java/com/rramirez/animals_service/controllers/AnimalController.java
@@ -1,10 +1,13 @@
 package com.rramirez.animals_service.controllers;
 
+import com.rramirez.animals_service.dtos.AnimaisPorFuncionario;
 import com.rramirez.animals_service.entidades.Animal;
 import com.rramirez.animals_service.repositorios.AnimalReposiory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -26,5 +29,30 @@ public class AnimalController {
     @PostMapping
     private Animal cadastrarAnimal(@RequestBody Animal animal){
         return reposiory.save(animal);
+    }
+
+    @GetMapping("/nao-adotados")
+    private List<Animal> listaNaoAdotados(){
+        return reposiory.findNotAdoped();
+    }
+
+    @GetMapping("/adotados")
+    private List<Animal> listaAdotados(){
+        return reposiory.findAdoped();
+    }
+
+    @GetMapping("/cachorros")
+    private List<Animal> listarDogs(){
+        return  reposiory.findTipoDog();
+    }
+
+    @GetMapping("/gatos")
+    private List<Animal> listarCats(){
+        return  reposiory.findTipoCat();
+    }
+
+    @GetMapping("/resgates")
+    public List<AnimaisPorFuncionario> listaPorFuncionario() {
+        return reposiory.findAnimaisPorFuncionario(LocalDateTime.now().minusYears(1), LocalDateTime.now());
     }
 }
